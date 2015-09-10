f5f26253c41aa28bc408ed3bd2f4e9ddf64e7932
Timothy George Lamballais Tessensohn <timothy.lamballais@booking.com>
2015-09-10 12:38:29 +0200

    New tag.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 setup(
     name='pdnsbe',
     packages=['pdnsbe'],
-    version='0.1',
+    version='0.2',
     description="""Library to easily implement and run a python PowerDNS socket
                    backend.""",
     author='Peter Downs',
     author_email='peterldowns@gmail.com',
     url='https://github.com/wimtie/pdnsbe',
-    download_url='https://github.com/wimtie/pdnsbe/tarball/0.1',
+    download_url='https://github.com/wimtie/pdnsbe/tarball/0.2',
     keywords=['PowerDNS', 'backend', 'python', 'generic', 'pluggable'],
     classifiers=[],
 )
